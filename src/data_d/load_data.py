a75ad0af3a1f389be90874ff7acfa3e26f198602
Nihal Naser Siddiqui <nihal070502@gmail.com>
2026-06-01 23:36:32 +0530

    Ran full pipeline. Ran completely fine with the model saved

--- src/data_d/load_data.py
+++ src/data_d/load_data.py
@@ -13,5 +13,4 @@ def load_data(file_path: str) -> pd.DataFrame:
     """
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
-    
     return pd.read_csv(file_path)
